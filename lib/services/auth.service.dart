2f79eb7496bb9a08945d29fb7040884531071166
timaevV <timaevvladislav@gmail.com>
2023-12-01 02:36:32 +0600

    feat:auth endpoints

--- lib/services/auth.service.dart
+++ lib/services/auth.service.dart
@@ -3,12 +3,12 @@ import "package:flutter_todo_auth/api.dart";
 class Authentication {
 
   Future register(email, password) async {
-    final user = await ApiClient.post("", {});
+    final user = await ApiClient.post("/api/auth/registration", {});
     return user;
   }
 
   Future login(email, password) async {
-    final user = await ApiClient.post("", {});
+    final user = await ApiClient.post("/api/auth/login", {});
     return user;
   }
 
