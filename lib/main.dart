efd033a96feb8b6a9637806edb1a52aa9cb7ca88
timaevV <timaevvladislav@gmail.com>
2023-12-03 17:50:06 +0600

    feat:register provider

--- lib/main.dart
+++ lib/main.dart
@@ -4,20 +4,33 @@ import "package:flutter_todo_auth/screens/welcome.dart";
 import "package:flutter_todo_auth/screens/home.dart";
 import "package:flutter_todo_auth/screens/login.dart";
 import "package:flutter_todo_auth/screens/registration.dart";
+import "package:flutter_todo_auth/store/tasks.dart";
+import "package:provider/provider.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(FlashChat());
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => Tasks()), // Replace with your actual data model class
+      ],
+      child: Todo()
+    )
+  );
 }
 
-class FlashChat extends StatelessWidget {
+class Todo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(initialRoute: WelcomeScreen.route, routes: {
-      WelcomeScreen.route: (context) => WelcomeScreen(),
-      RegistrationScreen.route: (context) => RegistrationScreen(),
-      LoginScreen.route: (context) => LoginScreen(),
-      HomeScreen.route: (context) => HomeScreen()
-    });
+    return MaterialApp(
+      initialRoute: WelcomeScreen.route,
+      routes: {
+        WelcomeScreen.route: (context) => WelcomeScreen(),
+        RegistrationScreen.route: (context) => RegistrationScreen(),
+        LoginScreen.route: (context) => LoginScreen(),
+        HomeScreen.route: (context) => HomeScreen(),
+
+      }
+    );
   }
 }
