96e93af19684ea6ee8257f2bf6a98a7db79f8074
timaevV <timaevvladislav@gmail.com>
2023-12-05 03:08:55 +0600

    feat:login access token

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,9 +1,10 @@
 import "package:flutter/material.dart";
+import "package:flutter_todo_auth/store/auth.dart";
+import "package:shared_preferences/shared_preferences.dart";
 
 import "package:flutter_todo_auth/screens/home.dart";
-import "package:flutter_todo_auth/services/auth.service.dart";
-import "package:flutter_todo_auth/widgets/button.dart";
-import "package:flutter_todo_auth/widgets/input.dart";
+import "package:flutter_todo_auth/widgets/button_auth.dart";
+import "package:flutter_todo_auth/widgets/input_auth.dart";
 import "package:flutter_todo_auth/widgets/animations/logo.dart";
 import "package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart";
 
@@ -20,6 +21,7 @@ class _LoginScreenState extends State<LoginScreen> {
   bool loading = false;
   String email = "";
   String password = "";
+  late String message = "";
 
   void setLoading(bool state) {
     setState(() {
@@ -27,44 +29,81 @@ class _LoginScreenState extends State<LoginScreen> {
     });
   }
 
+  void loginHandler() async {
+    try {
+      setLoading(true);
+      final response = await auth.login(email, password);
+      final String? token = response["token"];
+
+      if (token != null && token.isNotEmpty) {
+        accessTokenHandler(token);
+        Navigator.pushNamed(context, HomeScreen.route);
+      }
+
+      errorMessageHandler(response["message"]);
+    } catch (e) {
+      setLoading(false);
+    }
+  }
+
+  void accessTokenHandler(String token) async {
+    SharedPreferences.setMockInitialValues({});
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString("access_token", token);
+    message = "";
+    setLoading(false);
+  }
+
+  void errorMessageHandler(String msg) {
+    setLoading(false);
+    message = msg;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: ModalProgressHUD(
-          inAsyncCall: loading,
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 24.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                LogoAnimation(),
-                SizedBox(height: 48.0),
-                Input(field: email, label: "Enter your email", obscure: false),
-                SizedBox(height: 8.0),
-                Input(field: password, label: "Enter your password", obscure: true),
-                SizedBox(height: 24.0),
-                RoundedButton(
-                    title: "Log In",
-                    color: Colors.lightBlueAccent,
-                    onPressed: () async {
-                      try {
-                        setLoading(true);
-                        final user = await auth.login(email, password);
-
-                        if (user.token) {
-                          setLoading(false);
-                          Navigator.pushNamed(context, HomeScreen.route);
+        backgroundColor: Colors.white,
+        body: ModalProgressHUD(
+            inAsyncCall: loading,
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 24.0),
+                child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: <Widget>[
+                      LogoAnimation(),
+                      Center(child: Text(message, style: TextStyle(color: Colors.redAccent))),
+                      SizedBox(height: 48.0),
+                      Input(
+                        label: "Enter your email",
+                        obscure: false,
+                        onChanged: (value) {
+                          setState(() {
+                            email = value;
+                          });
                         }
-                      } catch (e) {
-                        setLoading(false);
-                      }
-                    }),
-              ]
+                      ),
+                      SizedBox(height: 8.0),
+                      Input(
+                        label: "Enter your password",
+                        obscure: true,
+                        onChanged: (value) {
+                          setState(() {
+                            password = value;
+                          });
+                        }
+                      ),
+                      SizedBox(height: 24.0),
+                      RoundedButton(
+                          title: "Log In",
+                          color: Colors.lightBlueAccent,
+                          onPressed: () async {
+                              loginHandler();
+                          })
+                    ]
+                )
             )
-          )
-      )
+        )
     );
   }
 }
